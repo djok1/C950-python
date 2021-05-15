546e1e13d979672cf586b970682cfa0f0655090b
djok1 <djok@live.com>
2021-05-15 13:40:00 -0600

    PCShift

--- classes/hashTable.py
+++ classes/hashTable.py
@@ -10,17 +10,21 @@ class hashTbl:
     # ended up deciding to use -1 to indicate there has been a value as 1 == true returns true
     def add(self, curentPackage):
         curentPackage.hash = self._get_hash(curentPackage.id)
-        if(self[curentPackage.hash] == False or self[curentPackage.hash] == -1):
-            self[curentPackage.hash] = curentPackage
+        if(self.map[curentPackage.hash] == False or self.map[curentPackage.hash] == -1):
+            self.map[curentPackage.hash] = curentPackage
         else:
             i = 1
             size = self.size - 1
             while i <= self.size:
                 if(i + curentPackage.hash> size):
-                    if(not(self[i - curentPackage.hash]) or self[i - curentPackage.hash] == -1):
-                        self[i - curentPackage.hash] = curentPackage
+                    if(not(self.map[i - curentPackage.hash]) or self.map[i - curentPackage.hash] == -1):
+                        self.map[i - curentPackage.hash] = curentPackage
+                        break
                 else:
-                    if(not(self[i + curentPackage.hash]) or self[i + curentPackage.hash] == -1):
-                        self[i + curentPackage.hash] = curentPackage
+                    if(not(self.map[i + curentPackage.hash]) or self.map[i + curentPackage.hash] == -1):
+                        self.map[i + curentPackage.hash] = curentPackage
+                        break
+                i += 1
+                        
     def passtest(curentPackage):
         curentPackage.id = 10
--- main.py
+++ main.py
@@ -6,7 +6,14 @@ c1.id = 0
 print(c1.id)
 hashTbl.passtest(c1)
 print(c1.id)
-c2 = hashTbl(1)
+c2 = hashTbl(10)
+c2.add(c1)
+c3 = package()
+c3.id = 5
+c4 = package()
+c4.id = 15
+c2.add(c3)
+c2.add(c4)
 print(c2.size)
 print(c2.map)
 input()
