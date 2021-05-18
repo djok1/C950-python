f0d4db0bba85d46eca4e4264c821e92407b7595c
djok1 <djok@live.com>
2021-05-17 21:19:37 -0600

    finished hash table worked on package class figured out how to read csv

--- classes/hashTable.py
+++ classes/hashTable.py
@@ -25,6 +25,49 @@ class hashTbl:
                         self.map[i + curentPackage.hash] = curentPackage
                         break
                 i += 1
+            if(i > self.size):
+                print("error:Table overflow")
                         
     def passtest(curentPackage):
         curentPackage.id = 10
+
+    def _print(self):
+        i = 0
+        print('current package ids')
+        while i < self.size:
+            if(not(self.map[i]) or self.map[i] == -1):
+                print(self.map[i])
+            else:
+                print(self.map[i].id)
+            i += 1
+    
+    def HashRemove(self, id):
+        location = self.searchById(id)
+        remove = self.map[location]
+        remove._print()
+        print( " removed")
+        self.map[location] = -1
+        
+        return remove
+    
+    def searchById(self, id):
+        idHash = self._get_hash(id)
+        i = 0
+        size = self.size - 1
+        while i <= self.size:
+            if(i + idHash > size):
+                if(self.map[i - idHash].id==idHash):
+                    return i - idHash
+                    break
+                elif(not(self.map[i - idHash])):
+                    break
+            else:
+                if(self.map[i + idHash].id==idHash):
+                    return i + idHash
+                    break
+                elif(not(self.map[i - idHash])):
+                    break
+            
+            i += 1
+        print("id not found")
+        return -1
--- classes/package.py
+++ classes/package.py
@@ -1,3 +1,11 @@
 class package:
     def __init__(self):
         self.id = -1
+        self.address = None
+        self.City = None
+        self.State = None
+        self.Zip = None
+        self.DeliveryDeadline = None
+        self.SpecialNotes = None
+    def _print(self):
+        print(self.id, end='')
